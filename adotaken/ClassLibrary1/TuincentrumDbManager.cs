47985a5cb44e58470e93d8332d0d58329cfa5a37
Chris Huys <chrishuys@live.com>
2017-03-15 14:19:52 +0100

    opgave 2

---
+++ adotaken/ClassLibrary1/TuincentrumDbManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+
+namespace TakenGemeenschap
+{
+    public class TuincentrumDbManager
+    {
+        private static ConnectionStringSettings conTuincentrumString = ConfigurationManager.ConnectionStrings["Tuincentrum"];
+
+        private static DbProviderFactory factory = DbProviderFactories.GetFactory(conTuincentrumString.ProviderName);
+
+        public DbConnection GetConnection()
+        {
+
+            var conTuincentrum = factory.CreateConnection();
+
+            conTuincentrum.ConnectionString = conTuincentrumString.ConnectionString;
+            return conTuincentrum;
+
+        }
+    }
+}
